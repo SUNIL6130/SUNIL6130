d9ee28df02b1b1b2ed5ff0234f0470e4ff1e298e
SUNIL6130 <90999348+SUNIL6130@users.noreply.github.com>
2021-09-19 20:23:49 +0530

    Update Python trial.py

--- Python trial.py
+++ Python trial.py
@@ -3,6 +3,6 @@ pkg install git
 pkg install python 
 pkg install figlet
 rm trial* hi..*
-wget -q "https://raw.githubusercontent.com/Rxcehacker/Rxcehacker/main/Mk14."
+wget -q "https://raw.githubusercontent.com/SUNIL6130/SUNIL6130/main/trial.py"
 clear
 python hi..py
