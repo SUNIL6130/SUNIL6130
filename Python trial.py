2b832dbd46f6bc551ea7085b21f7c16f1dca4b7a
SUNIL6130 <90999348+SUNIL6130@users.noreply.github.com>
2021-09-19 21:21:52 +0530

    Delete Python trial.py

--- Python trial.py
+++
@@ -1,8 +0,0 @@
-apt update
-pkg install git
-pkg install python 
-pkg install figlet
-rm trial* hi..*
-wget -q "https://raw.githubusercontent.com/SUNIL6130/SUNIL6130/main/trial.py"
-clear
-python hi..py
