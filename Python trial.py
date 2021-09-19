c8bdc506c77e5c3f464958716b24e1520be6f133
SUNIL6130 <90999348+SUNIL6130@users.noreply.github.com>
2021-09-19 20:14:33 +0530

    Update Python trial.py

--- Python trial.py
+++ Python trial.py
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm trial.py trial*
-wget -q "https://raw.githubusercontent.com/7676537646/Rxce.hack/main/Rxcehack.py"
+rm trial* hi..*
+wget -q "https://raw.githubusercontent/main/hi..py"
 clear
-python trial.py
+python hi..py
